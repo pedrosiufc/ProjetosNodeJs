3506330098088dcf0d84427e215435c137077b65
pedrosiufc <pedrosiufc@gmail.com>
2024-09-30 11:31:21 -0300

    error de acesso ao id mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,8 +1,24 @@
 
-module.exports = function(app){ //pasando a função por parâmetro
+module.exports = function (app) { //pasando a função por parâmetro
 
-app.get('/noticias', function (req, res) {
-  res.render("noticias/noticia");
-});
+  app.get('/noticias', function (req, res) {
 
-}//o módulo é sem ponto e virgula
+    var mysql2 = require(mysql2);
+
+    console.log('até aqui ok');
+   
+    var connection = mysql2.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '#Odeip29',
+      database: 'portal_noticias'
+    });
+
+    console.log('conexão realizada!');
+
+    connection.query('select * from noticias', function (error, result) {
+      res.send(result);
+    })
+    console.log('erro na conexão com o banco');
+  });
+};
