eaac05f11367967659db8f41e9ef669a7a124cfb
pedrosiufc <pedrosiufc@gmail.com>
2024-09-30 20:04:54 -0300

    Criando a página detalhes da notícia

---
+++ app/routes/noticia.js
@@ -0,0 +1,11 @@
+module.exports = function (app) {
+
+  app.get('/noticia', function (req, res) {
+
+    var connection = app.config.dbConnection();
+
+    connection.query("SELECT * FROM noticias where id_noticia = 2", function (error, result) {
+      res.render("noticias/noticia", { noticia: result });
+    });
+  });
+};
