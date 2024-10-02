971bf4f035153ae9b44836aeedc504edd73079c8
pedrosiufc <pedrosiufc@gmail.com>
2024-10-02 19:44:07 -0300

    Expandindo formulário e fazendo a validação com express-validator

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -11,7 +11,9 @@ NoticiasDAO.prototype.getNoticia = function (callback) {
   this._connection.query("SELECT * FROM noticias where id_noticia = 2", callback);
 }
 
+//a persistência fica automática pois os names dados são iguais aos que estão no bd, passados através de JSON
 NoticiasDAO.prototype.salvarNoticia = function (noticia, callback) {
+  console.log(noticia); //mostra o conteúdo inserido
   //transforma o json em string passando para inserir  no BD com SET
   this._connection.query('insert into noticias set ?', noticia, callback)
 }
