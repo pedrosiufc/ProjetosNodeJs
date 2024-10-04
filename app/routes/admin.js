cd1cb985555a9a325c0496724419bd3c671363c1
pedrosiufc <pedrosiufc@gmail.com>
2024-10-04 16:03:29 -0300

    Mantendo os dados no fomulário com utilização do express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,25 +2,25 @@
 module.exports = function (application) {
 
   application.get('/formulario_inclusao_noticia', function (req, res) {
-    res.render("admin/form_add_noticia");
+    res.render("admin/form_add_noticia", {validacao:{}, noticia: {}});//passando o parametro para a view para não dar erro no form
   });
 
   application.post('/noticias/salvar', function (req, res) { //informando req via post
     var noticia = req.body;
 
     //tratando validação de dados com express-validator
-   // console.log(noticia);
-    req.assert('titulo','Título é obrigatório').notEmpty(); 
-    req.assert('resumo','Resumo é obrigatório').notEmpty(); 
-    req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10,100); 
-    req.assert('autor','O Autor é obrigatório').notEmpty(); 
-    req.assert('data_noticia','A data é obrigatória').notEmpty().isDate({format: 'YYYY-MM-DD'}); 
-    req.assert('noticia','A notícia é obrigatória').notEmpty(); 
+    // console.log(noticia);
+    req.assert('titulo', 'Título é obrigatório').notEmpty();
+    req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+    req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+    req.assert('autor', 'O Autor é obrigatório').notEmpty();
+    req.assert('data_noticia', 'A data é obrigatória').notEmpty().isDate({ format: 'YYYY-MM-DD' });
+    req.assert('noticia', 'A notícia é obrigatória').notEmpty();
 
     var erros = req.validationErrors();
-
-    if(erros){
-      res.render("admin/form_add_noticia"); //caso erro true retorna a página de inclusão.
+   
+    if (erros) {
+      res.render("admin/form_add_noticia", { validacao: erros, noticia: noticia}); 
       return;
     }
 
