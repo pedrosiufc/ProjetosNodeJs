24f2a980cd16b59136f1ad5c88588078e02253ce
pedrosiufc <pedrosiufc@gmail.com>
2024-10-07 09:58:11 -0300

    Aplicando busca no bd e reinderizando na view as 5 últimas noticias

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,12 @@
 module.exports.index = function(application, req, res){
-  res.render("home/index");
+  
+  var connection = application.config.dbConnection();
+  var noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+  noticiasModel.get5UltimasNoticias(function(error, result){
+    console.log(result);
+    //passando para view através de json
+    res.render("home/index", {noticias : result});
+  });
+  
 }
